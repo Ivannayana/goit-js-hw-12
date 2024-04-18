c10ca21422a50cfd9bf4c8d1c67be4eba1b47452
Ivannayana <151822259+Ivannayana@users.noreply.github.com>
2024-04-18 17:44:41 +0000

    Deploying to gh-pages from @ Ivannayana/goit-js-hw-12@602f62c3f8bad0d718f9701880d95d3796b12b4d 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,17 +1,17 @@
-import{S as m,i as f}from"./assets/vendor-8c59ed88.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&i(a)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();function p(s){return s.map(({webformatURL:o,largeImageURL:r,tags:i,likes:e,views:t,comments:a,downloads:u})=>`<li class="gallery-item">
-	<a class="gallery-link" href="${r}">
+import{a as m,S as d,i as p}from"./assets/vendor-6e0bf343.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&a(i)}).observe(document,{childList:!0,subtree:!0});function t(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(e){if(e.ep)return;e.ep=!0;const r=t(e);fetch(e.href,r)}})();function f(s){return s.map(({webformatURL:o,largeImageURL:t,tags:a,likes:e,views:r,comments:i,downloads:u})=>`<li class="gallery-item">
+	<a class="gallery-link" href="${t}">
 		<img 
 			class="gallery-image" 
 			src="${o}"
-			alt="${i}"
+			alt="${a}"
 			width="360px"
             height="152px" />
 	</a>
     <ul class="image-text"> 
     <li class="value">Likes<p class="amount">${e}</p></li>
-    <li class="value">Views<p class="amount">${t}</p></li>
-    <li class="value">Comments<p class="amount">${a}</p></li>
+    <li class="value">Views<p class="amount">${r}</p></li>
+    <li class="value">Comments<p class="amount">${i}</p></li>
     <li class="value">Downloads<p class="amount">${u}</p></li>
     </ul>
-   </li>`).join("")}const d="43312090-89e3d2b4f295c74d82d728d94";function g(s){const o=new URLSearchParams({key:d,q:s,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:99});return fetch(`https://pixabay.com/api/?${o}`).then(r=>{if(!r.ok)throw new Error(r.statusText);return r.json()})}const h=new m(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250}),c=document.querySelector("form"),l=document.querySelector(".gallery"),n=document.querySelector("#animation");c.addEventListener("submit",y);function y(s){s.preventDefault(),n.classList.toggle("loader"),l.innerHTML="";const o=s.target.elements.text.value.trim();o&&g(o).then(r=>{r.total===0?f.error({message:"Sorry, there are no images matching your search query. Please try again!"}):(l.innerHTML=p(r.hits),h.refresh()),n.classList.toggle("loader"),c.reset()}).catch(r=>console.log(r))}
+   </li>`).join("")}const g="43312090-89e3d2b4f295c74d82d728d94";async function y(s){const o=new URLSearchParams({key:g,q:s,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:99});try{const t=await m.get(`https://pixabay.com/api/?${o}`);if(!t.ok)throw new Error(t.statusText);return await t.json()}catch(t){throw console.error("Помилка отримання даних:",t),t}}const h=new d(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250}),c=document.querySelector("form"),l=document.querySelector(".gallery"),n=document.querySelector("#animation");document.querySelector("js-loade-more");c.addEventListener("submit",L);function L(s){s.preventDefault(),n.classList.toggle("loader"),l.innerHTML="";const o=s.target.elements.text.value.trim();o&&y(o).then(t=>{t.total===0?p.error({message:"Sorry, there are no images matching your search query. Please try again!"}):(l.innerHTML=f(t.hits),h.refresh()),n.classList.toggle("loader"),c.reset()}).catch(t=>console.log(t))}
 //# sourceMappingURL=commonHelpers.js.map
